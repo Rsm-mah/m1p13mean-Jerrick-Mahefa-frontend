acbb4ba0ce0bd534d1354485ee23e39fde9d1749
Rsm-mah <mahrasamisoa@gmail.com>
2026-02-28 14:39:13 +0300

    deploy

--- public/env.js
+++ public/env.js
@@ -1,5 +1,5 @@
 (function (window) {
   window.__env = window.__env || {};
-  // window.__env.API_URL = 'm1p13mean-jerrick-mahefa-backend.vercel.app';
-  window.__env.API_URL = 'http://localhost:3000';
+  window.__env.API_URL = 'm1p13mean-jerrick-mahefa-backend.vercel.app';
+  // window.__env.API_URL = 'http://localhost:3000';
 }(this));
